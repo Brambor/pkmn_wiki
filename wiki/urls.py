3991e48e656af94f124c134fdffc305933c7eda1
Brambor <tondadrd@seznam.cz>
2016-08-04 23:59:06 +0200

    Build 3
    finally working, but still not useful

--- wiki/urls.py
+++ wiki/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 app_name = 'wiki'
 urlpatterns = [
     url(r'^$', views.index_view),
@@ -10,4 +13,4 @@ urlpatterns = [
     url(r'^aoe/$', views.edit_view),
     url(r'^aoe/(?P<action>\w+)/$', views.edit_view),
     url(r'^aoe/(?P<action>\w+)/(?P<pokedex>[0-9]+)/$', views.edit_view),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
