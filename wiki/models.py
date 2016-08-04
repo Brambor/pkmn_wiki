29df577e3fd81d71a25b4d4191442fc2d8d1b453
Brambor <tondadrd@seznam.cz>
2016-08-04 23:36:31 +0200

    Build 2

--- wiki/models.py
+++ wiki/models.py
@@ -9,7 +9,7 @@ class Pokemons(models.Model):
 	evolve_from_fk = models.ForeignKey("Pokemons", related_name="from_fk", null=True, blank=True)
 	evolve_to_at = models.IntegerField(default=0)
 	evolve_to_fk = models.ForeignKey("Pokemons", related_name="to_fk", null=True, blank=True)
-	another_img = models.ImageField(null=True, default=None, upload_to=settings.MEDIA_ROOT + "images/", blank = True)
+	another_img = models.ImageField(null=True, default=None, blank = True)
 	#target = models.ForeignKey(User, related_name='gameclaim_targets')
 	#joke (ivysaur/bulbasaur/venusaur: <img>how he should look; if leaves are missing let him out of pokeball for sunlight)
 	#pokemon lore
