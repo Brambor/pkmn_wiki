059a4202b83787e8571bb93af59487ee817c2c56
Brambor <tondadrd@seznam.cz>
2016-08-09 23:45:06 +0200

    Build 8 - preparing for History
    -Bugfix
    ---deleting a pokemon will no longer delete everything related to it and
    related to "it" and so on, ups
    -New model Change now remembers every change, creation and forget it all
    on deletion.
    -History page is not jet done, so user cannot see it
    -(deleted all .pyc files, sorry :))

--- wiki/forms.py
+++ wiki/forms.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.forms import ModelForm
 
 from .models import Pokemons
@@ -6,4 +5,4 @@ from .models import Pokemons
 class PokemonForm(ModelForm):
 	class Meta:
 		model = Pokemons
-		fields = ["pokedex", "pokemon_name", "image", "evolve_from_at", "evolve_from_fk", "evolve_to_at", "evolve_to_fk"]
+		fields = ["pokedex", "name", "image", "evolve_from_at", "evolve_from_fk", "evolve_to_at", "evolve_to_fk"]
--- wiki/models.py
+++ wiki/models.py
@@ -3,12 +3,11 @@ from django.conf import settings
 
 class Pokemons(models.Model):
 	pokedex = models.IntegerField(default=0, unique=True)
-	pokemon_name = models.CharField(max_length=20)
-	pokemon_img = models.CharField(max_length=24)
+	name = models.CharField(max_length=20)
 	evolve_from_at = models.IntegerField(default=0)
-	evolve_from_fk = models.ForeignKey("Pokemons", related_name="from_fk", null=True, blank=True)
+	evolve_from_fk = models.ForeignKey("Pokemons", related_name="from_fk", null=True, blank=True, on_delete=models.SET_NULL)
 	evolve_to_at = models.IntegerField(default=0)
-	evolve_to_fk = models.ForeignKey("Pokemons", related_name="to_fk", null=True, blank=True)
+	evolve_to_fk = models.ForeignKey("Pokemons", related_name="to_fk", null=True, blank=True, on_delete=models.SET_NULL)
 	image = models.ImageField(null=True, default=None, blank = True)
 	#target = models.ForeignKey(User, related_name='gameclaim_targets')
 	#joke (ivysaur/bulbasaur/venusaur: <img>how he should look; if leaves are missing let him out of pokeball for sunlight)
@@ -16,6 +15,25 @@ class Pokemons(models.Model):
 	#pokemon stats
 	#pokemon on map
 	def __str__(self):
-		return self.pokemon_name
+		return self.name
 	def get_img(self):
 		return self.image.url
+	def save(self, **kwargs):
+		attributes = ["pokedex", "name", "image", "evolve_from_at", "evolve_from_fk", "evolve_to_at", "evolve_to_fk"]
+		before = Change()
+		for atr in attributes:
+			setattr(before, atr, getattr(self, atr))
+		before.save()
+		super().save(**kwargs)
+
+class Change(models.Model):
+	pokemon = Pokemons()
+	time = models.DateTimeField(auto_now_add=True)
+
+	pokedex = models.IntegerField(default=0)
+	name = models.CharField(max_length=20)
+	evolve_from_at = models.IntegerField(default=0)
+	evolve_from_fk = models.ForeignKey("Pokemons", related_name="from_ch", null=True, blank=True, on_delete=models.SET_NULL)
+	evolve_to_at = models.IntegerField(default=0)
+	evolve_to_fk = models.ForeignKey("Pokemons", related_name="to_ch", null=True, blank=True, on_delete=models.SET_NULL)
+	image = models.ImageField(null=True, default=None, blank = True)
